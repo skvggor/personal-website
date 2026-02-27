dc0a037934f26e9fa3964446e3d6e371fe62cf6b
Marcos Garcia de Lima <marckfree@gmail.com>
2026-02-27 12:52:04 -0300

    Update social icons with dashboardicons and improve hover effects

--- components/social/social.d.ts
+++ components/social/social.d.ts
@@ -1,10 +1,8 @@
-import type { JSX } from "react";
-
 export interface ISocial {
   links: {
     id: number;
     title: string;
     href: string;
-    icon: JSX.Element;
+    icon: string;
   }[];
 }
--- components/social/social.tsx
+++ components/social/social.tsx
@@ -1,9 +1,4 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import SkateboardingIcon from "@mui/icons-material/Skateboarding";
-import SourceIcon from "@mui/icons-material/Source";
-import SpeakerIcon from "@mui/icons-material/Speaker";
-import XIcon from "@mui/icons-material/X";
+import Image from "next/image";
 
 import type { ISocial } from "@/components/social/social.d";
 
@@ -12,87 +7,27 @@ export default function Social() {
     links: [
       {
         id: 1,
-        title: "GitHub",
-        href: "https://github.com/skvggor",
-        icon: (
-          <GitHubIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
+        title: "YouTube",
+        href: "https://youtube.com/@skvggor",
+        icon: "/icons/youtube.svg",
       },
       {
         id: 2,
-        title: "LeetCode",
-        href: "https://leetcode.com/skvggor/",
-        icon: (
-          <SourceIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
+        title: "GitHub",
+        href: "https://github.com/skvggor",
+        icon: "/icons/github.svg",
       },
       {
         id: 3,
         title: "LinkedIn",
         href: "https://www.linkedin.com/in/marcker",
-        icon: (
-          <LinkedInIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
+        icon: "/icons/linkedin.svg",
       },
       {
         id: 4,
-        title: "Twitter",
-        href: "https://twitter.com/skvggor",
-        icon: (
-          <XIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
-      },
-      {
-        id: 5,
         title: "Strava",
         href: "https://www.strava.com/athletes/18616728",
-        icon: (
-          <SkateboardingIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
-      },
-      {
-        id: 6,
-        title: "Last.fm",
-        href: "https://last.fm/user/skvggor",
-        icon: (
-          <SpeakerIcon
-            sx={{
-              color: "#0ea5e9",
-            }}
-            width={24}
-            height={24}
-          />
-        ),
+        icon: "/icons/strava.svg",
       },
     ],
   };
@@ -103,23 +38,19 @@ export default function Social() {
       className="item"
     >
       <a
-        className="link
-          block
-          duration-500
-          hover:outline-white
-          outline
-          outline-2
-          outline-sky-500
-          outline-offset-1
-          p-2
-          rounded-full
-          transition-all"
+        className="link block p-2 rounded-full transition-all"
         href={link.href}
         title={link.title}
         target="_blank"
         rel="noopener noreferrer"
       >
-        {link.icon}
+        <Image
+          src={link.icon}
+          alt={link.title}
+          width={28}
+          height={28}
+          className="w-7 h-7 transition-transform duration-300 hover:scale-110"
+        />
       </a>
     </li>
   ));
@@ -136,7 +67,7 @@ export default function Social() {
         className="icon-list
           gap-x-5
           grid
-          grid-cols-6
+          grid-cols-4
           md:gap-x-6"
       >
         {renderSocialLinks}
